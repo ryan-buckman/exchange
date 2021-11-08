1f89a535ab3fc6f06f82305ea83ffb40d1a2f7a1
ryanbuckman <ryanbuckman@outlook.com>
2021-11-08 14:13:53 +0000

    feat: added navbar and page heading styling

---
+++ web/src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Nav from '$lib/layout/Nav.svelte';
+	import PageHeading from '$lib/layout/PageHeading.svelte';
+	import '../app.css';
+</script>
+
+<div class="h-full w-full absolute">
+    <Nav></Nav>      
+    <PageHeading></PageHeading>
+    <div class="container mx-auto px-6">
+        <!-- Remove class [ h-64 ] when adding a card block -->
+        <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
+        <div class="w-full h-64 rounded">             
+               <slot></slot>       
+        </div>
+    </div>
+</div>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+    <h3 class="text-lg font-medium leading-6 text-indigo-700">Welcome to Exchange built with SvelteKit</h3>
+    <p class="mt-1 text-sm text-gray-700">Visit <a class="text-indigo-600 hover:text-indigo-700" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
