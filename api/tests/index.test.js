1c1dea5eb11dc9e1e28ef4798986c341ee15a528
ryanbuckman <ryanbuckman@outlook.com>
2021-11-24 09:46:21 +0000

    feat: added first server test

--- api/tests/index.test.js
+++ api/tests/index.test.js
@@ -1,1 +1,11 @@
-//TODO: Add tests
+const config = require('../configuration/config');
+let server = require('../src/index');
+let chai = require('chai');
+
+let expect = chai.expect;
+
+describe('Server', ()=>{
+    it('tests that server is running current port', async()=>{
+        expect(server.port).to.equal(config.port) 
+    })
+});
