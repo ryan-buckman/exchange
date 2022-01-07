4dc7801048c1cedcfaa9b9cd9199cc9fe53839f5
ryanbuckman <ryanbuckman@outlook.com>
2022-01-07 09:55:05 +0000

    feat: updated dashboard content

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <h3 class="text-lg font-medium leading-6 text-indigo-700">
-	Welcome to Exchange built with SvelteKit
+	Welcome to Exchange built with SvelteKit - Search Stocks, Currencies and Crypto
 </h3>
 <p class="mt-1 text-sm text-gray-700">
 	Visit <a class="text-indigo-600 hover:text-indigo-700" href="https://kit.svelte.dev"
